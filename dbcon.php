45d85020f0f8d841222f1044f6833352ecdc347d
KevinGabari <99273021+KevinGabari@users.noreply.github.com>
2023-12-04 20:42:44 +0000

    Add files via upload

--- dbcon.php
+++ dbcon.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost";
 $username="root";
-$password="":
+$password="";
 $dbname="employees";
 //Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
