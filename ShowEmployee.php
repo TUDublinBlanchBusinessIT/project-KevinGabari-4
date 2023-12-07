f0244e827fb5f853656136265b973b0b14800187
KevinGabari <99273021+KevinGabari@users.noreply.github.com>
2023-12-07 22:29:08 +0000

    Add files via upload

--- ShowEmployee.php
+++ ShowEmployee.php
@@ -6,16 +6,18 @@ include("dbcon.php");
 $sql = "select * from employee";
 $result =mysqli_query($con,$sql);
 
-$result = mysqli_query($con, $sql);
-while($row = mysqli_fetch_assoc($result)) {
- $fn = $row['firstname'];
- $sn = $row['surname'];
- $dob = $row['dateofbirth'];
- $salary = $row['salary'];
 
-	echo "$fn, $sn, $dob, $salary<br>";
+echo "<Table border ='1'>";
+while($row = mysqli_fetch_assoc($result)) {
+	 $fn = $row['firstname'];
+	 $sn = $row['surname'];
+	 $dob = $row['dateofbirth'];
+	 $salary = $row['salary'];
+
+	echo "<TR><TD>$fn</TD><TD>$sn</TD><TD>$dob</TD><TD>$salary</TD></TR>";
 
 }
+echo "</Table>";
 
 mysqli_close($con);
 
