690b7b39aa1a44d51818a80399913200b3ecd92c
KevinGabari <99273021+KevinGabari@users.noreply.github.com>
2023-12-07 22:16:47 +0000

    Add files via upload

---
+++ ShowEmployee.php
@@ -0,0 +1,22 @@
+<?php
+
+
+include("dbcon.php");
+
+$sql = "select * from employee";
+
+$result = mysqli_query($con, $sql);
+while(mysqli_fetch_assoc($result)) {
+ $fn = $row['firstname'];
+ $sn = $row['surname'];
+ $dob = $row['dateofbirth'];
+ $salary = $row['salary'];
+
+	echo "$fn, $sn, $dob, $salary<br>";
+
+}
+
+mysqli_close($con);
+
+
+?>
